97b6a54d4dd86548d00e7c7985b88972569653e5
Shuery <2463253700@qq.com>
2025-04-26 20:11:09 +0800

    🐞 fix(diy-part2): fileName[] 不应该更改。

--- diy-part2.sh
+++ diy-part2.sh
@@ -171,7 +171,6 @@ sed -i -E \
   -e '/curl|filename/s/OpenWrt/ImmortalWrt/g' \
   -e '/curl|filename/s/openwrt/immortalwrt/g' \
   -e '/curl|filename/s/Openwrt/Immortalwrt/g' \
-  -e '/file[Nn]ame/s/\[7\]/\[11\]/g' \
   -e '/file[Nn]ame/s/0:7/0:11/g' \
   -e '/Check\s+whether\s+EFI\s+firmware/,/^\s+fi/ {
       /^\s+fi/a\  suffix="squashfs-sysupgrade.itb"
