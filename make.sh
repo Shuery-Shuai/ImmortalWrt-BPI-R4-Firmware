a449cd759f95ace40b097d029af52aba9f1c4edf
Shuery-Shuai <2463253700@qq.com>
2025-06-19 19:49:37 +0800

    ✨ feat(make): 本地测试编译脚本。

---
+++ make.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git restore .
+git pull
+bash ./diy-part1.sh
+./scripts/feeds update -a -f
+bash ./diy-part2.sh
+./scripts/feeds install -a -f
+make defconfig
+find dl -size -1024c -exec ls -l {} \;
+find dl -size -1024c -exec rm -f {} \;
+make download
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
+    make -j$(($(nproc) + 1)) V=sc
