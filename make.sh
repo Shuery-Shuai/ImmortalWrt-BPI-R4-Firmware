07ea55b33675b618f7c409e9b12918ecc2fe99ca
Shuery-Shuai <2463253700@qq.com>
2025-06-25 20:11:46 +0800

    ✨ feat(make): 在多线程 make 失败时切换至单线程并开启详细日志。

--- make.sh
+++ make.sh
@@ -17,4 +17,6 @@ find dl -size -1024c -exec ls -l {} \;
 find dl -size -1024c -exec rm -f {} \;
 make download
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-    make -j$(($(nproc) + 1)) V=sc
+    make -j$(($(nproc) + 1)) ||
+    make -j1 V=sc ||
+    echo "编译失败，请检查错误日志。"
