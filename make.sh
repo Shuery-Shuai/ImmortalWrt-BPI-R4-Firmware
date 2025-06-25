48811ae462e26837b816859d840ed4293cfd2df7
Shuery-Shuai <2463253700@qq.com>
2025-06-25 20:48:40 +0800

    🐞 fix(make): 编译前将 diy-part 及 gen index 复制至 immortalwrt 目录。

--- make.sh
+++ make.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 if [ -d "immortalwrt" ]; then
     echo "进入 'immortalwrt' 目录..."
+    cp diy-part*.sh immortalwrt/
+    cp generate-index.sh immortalwrt/
     cd immortalwrt
 elif [ "$(basename "$(pwd)")" != "immortalwrt" ]; then
-    git clone --depth 1 https://github.com/immortalwrt/immortalwrt.git
+    git clone --depth 1 https://github.com/immortalwrt/immortalwrt.git || {
+        echo "克隆 'immortalwrt' 仓库失败，请检查网络连接或仓库地址。"
+        exit 1
+    }
+    cp diy-part*.sh immortalwrt/
+    cp generate-index.sh immortalwrt/
     cd immortalwrt
 fi
 git restore .
