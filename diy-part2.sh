03f4e3c370496a31c309f8f38673be0970c1e923
Shuery-Shuai <2463253700@qq.com>
2026-08-07 00:39:50 +0800

    🐞 fix(diy-part2): 暂时使用固定版本。

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,13 +42,13 @@ modify_rust_build_config
 ensure_exec_permission 'files/usr/bin/restore-packages.sh'
 
 # 固定 dae 版本
-set_dae_version --auto-update --pre-release
+set_dae_version "2.0.0" "51e5fe169a36c03503d74f3cb93a00a4547e41b5a8bd957599bfac0094639061"
 
 # 修复 qbittorrent 依赖
 fix_qbittorrent_deps
 
 # 固定 fan2go 版本
-set_fan2go_version --auto-update
+set_fan2go_version "0.15.0" "86b352936d924688bdf62aacfb9169a3706fe427d4eccb06dd412ff78ce328b9"
 
 # 修复 libnl-tiny 编译警告
 fix_libnl_tiny_compile
